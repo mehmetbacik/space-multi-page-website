c49824a13e89d8842ffd975696989682fbdd3506
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-02-24 23:32:30 +0300

    Update
    Updated crew page

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,17 +14,17 @@ const swiper = new Swiper('.swiper', {
         },
         640: {
             spaceBetween: 30,
-            slidesPerView: 2,
+            slidesPerView: 1,
             slidesPerColumn: 1,
         },
         768: {
             spaceBetween: 30,
-            slidesPerView: 2,
+            slidesPerView: 1,
             slidesPerColumn: 1,
         },
         1024: {
             spaceBetween: 30,
-            slidesPerView: 3,
+            slidesPerView: 1,
         },
     }
   });
