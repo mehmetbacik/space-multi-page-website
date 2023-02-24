775fed6dfaa4b8fdaf8b4f26711636c2a6901e92
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-02-24 23:40:48 +0300

    Update
    Updated technology page

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@ const swiper = new Swiper('.swiper', {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },*/
+    pagination: {
+        el: ".swiper-pagination",
+    },
     autoplay: {
         delay: 3000,
     },
